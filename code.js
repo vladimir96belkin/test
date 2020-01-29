6195ca1022ccaa8caceb85b0832d3357fdddf9ac
Vladimir <vladbelkin22@gmail.com>
2020-01-29 23:53:36 +0300

    task is done

--- code.js
+++ code.js
@@ -1,3 +1,20 @@
 'use strict'
 
-console.log(4 + '= four');
+let money = prompt("Ваш бюджет на месяц?");
+let time = prompt("Введите дату в формате YYYY-MM-DD");
+let appData = {
+    myMoney: money,
+    myData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: {},
+    savings: false,
+};
+let firstAnswer = prompt( "Введите обязательную статью расходов в этом месяце", "");
+let secondAnswer = prompt( "Во сколько это обойдется?", "");
+appData.expenses[firstAnswer] = secondAnswer;
+console.log(appData.myMoney);
+console.log(appData.myData); 
+console.log(appData.expenses);
+let moneyOneDay = money/30; 
+alert ("Бюджет на 1 день = " + moneyOneDay);
